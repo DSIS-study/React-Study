f54b26c17e3e001d9685b6648d9f9024cfcf84b5
HyeongJun Kim <khj029956@naver.com>
2025-05-23 19:46:46 +0900

    0523

--- my-app/src/somin/somin_page.jsx
+++ my-app/src/somin/somin_page.jsx
@@ -1,3 +1,18 @@
 export default function SominPage() {
-  return <div>정소민</div>;
+  return (
+    <div>
+      <h1>안녕하세요. 저는 정소민입니다. </h1>
+      <h2>
+        {' '}
+        <span>컴퓨터공학과 25학번입니다.</span>
+        열심히 해 보겠습니다{' '}
+      </h2>
+      <p>
+        제 <strong>취미</strong>는 <a href="https://www.youtube.com/">유튜브</a>
+        보기 입니다
+      </p>
+      <h3>감사합니다</h3>
+      <h6>thank you</h6>{' '}
+    </div>
+  );
 }
--- my-app/src/yunseung/yunseung_page.jsx
+++ my-app/src/yunseung/yunseung_page.jsx
@@ -1,3 +1,14 @@
+import './yunseung_page.css';
+
 export default function YunseungPage() {
-  return <div>태윤승</div>;
+  return (
+    <div className="yunseung-container">
+      <h1>안녕하세요</h1>
+      <h2>저는 ai학과</h2>
+      <h3>25학번 태윤승입니다</h3>
+      <h4>이번에 디스이즈에 들어오게 됐습니다다</h4>
+      <h5>열심히 하겠습니다!</h5>
+      <h6>잘 부탁드립니다다</h6>
+    </div>
+  );
 }
