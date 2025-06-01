540acc32cf7c45547f66e9742ea9b842f5456389
tyunseung <tyunseung@gmail.com>
2025-06-01 16:49:59 +0900

    yunseung

--- my-app/src/yunseung/yunseung_page.jsx
+++ my-app/src/yunseung/yunseung_page.jsx
@@ -3,9 +3,9 @@ import './yunseung_page.css';
 export default function YunseungPage() {
   return (
     <div className="yunseung-container">
-      <h1>안녕하세요</h1>
+      <h1 className = "hello">안녕하세요</h1>
       <h2>저는 ai학과</h2>
-      <h3>25학번 태윤승입니다</h3>
+      <h3 className = "name">25학번 태윤승입니다</h3>
       <h4>이번에 디스이즈에 들어오게 됐습니다다</h4>
       <h5>열심히 하겠습니다!</h5>
       <h6>잘 부탁드립니다다</h6>
